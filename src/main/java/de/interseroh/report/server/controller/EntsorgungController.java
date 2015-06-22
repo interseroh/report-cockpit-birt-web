a6e93da0385de62cbbf3b04b6c4812c4ffd53571
idueppe <ingo.dueppe@dueppe.com>
2015-06-23 00:11:39 +0200

    Enhanced for new Thymeleaf views

--- src/main/java/de/interseroh/report/server/controller/EntsorgungController.java
+++ src/main/java/de/interseroh/report/server/controller/EntsorgungController.java
@@ -20,10 +20,6 @@
  */
 package de.interseroh.report.server.controller;
 
-import javax.inject.Inject;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-
 import org.apache.log4j.Logger;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
@@ -33,70 +29,82 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-@PropertySource({ "classpath:config.properties", "classpath:version.properties" })
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+@PropertySource({"classpath:config.properties", "classpath:version.properties"})
 @Controller
 public class EntsorgungController {
 
-	private static final Logger logger = Logger
-			.getLogger(EntsorgungController.class);
+    private static final Logger logger = Logger.getLogger(EntsorgungController.class);
 
-	@Inject
-	private Environment env;
+    @Inject
+    private Environment env;
 
-	@RequestMapping(value = { "/home" }, method = RequestMethod.GET)
-	public ModelAndView homePage() {
-		logger.debug("Home executed");
+    @RequestMapping(value = {"/index"}, method = RequestMethod.GET)
+    public ModelAndView homePage() {
+        logger.debug("Home executed");
 
-		ModelAndView model = new ModelAndView();
-		model.setViewName("/home");
+        ModelAndView model = new ModelAndView();
+        model.setViewName("/home");
 
-		String interserohText = env
-				.getProperty("text.interseroh.reportcockpit");
-		if (interserohText == null || interserohText.equals("")) {
-			interserohText = "ReportingCockpit";
-		}
-		model.addObject("interserohText", interserohText);
+        String interserohText = env
+                .getProperty("text.interseroh.reportcockpit");
+        if (interserohText == null || interserohText.equals("")) {
+            interserohText = "ReportingCockpit";
+        }
+        model.addObject("interserohText", interserohText);
 
-		String version = env.getProperty("version");
-		model.addObject("version", version);
+        String version = env.getProperty("version");
+        model.addObject("version", version);
 
-		return model;
-	}
+        return model;
+    }
 
-	@RequestMapping(value = { "/*", "/entsorgung" }, method = RequestMethod.GET)
-	public ModelAndView entsorgungPage() {
-		logger.debug("Entsorgung executed");
+    @RequestMapping(value = {"/index"}, method = RequestMethod.GET)
+    public ModelAndView index() {
+        logger.debug("Index view executed");
 
-		ModelAndView model = new ModelAndView();
-		model.setViewName("/entsorgung");
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("/index");
+        return modelAndView;
+    }
 
-		return model;
-	}
+    @RequestMapping(value = {"/entsorgung"}, method = RequestMethod.GET)
+    public ModelAndView entsorgungPage() {
+        logger.debug("Entsorgung executed");
 
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public ModelAndView login(
-			@RequestParam(value = "error", required = false) String error,
-			@RequestParam(value = "logout", required = false) String logout,
-			@RequestParam(value = "autherror", required = false) String authError,
-			HttpServletRequest request) throws ServletException {
-		logger.debug("Login executed");
+        ModelAndView model = new ModelAndView();
+        model.setViewName("/entsorgung");
 
-		ModelAndView model = new ModelAndView();
-		if (error != null) {
-			model.addObject("error", "Benutzername und/oder Passwort falsch!");
-		}
-		if (logout != null) {
-			request.logout();
-			model.addObject("msg", "Logout war erfolgreich.");
-		}
-		if (authError != null) {
-			request.logout();
-			model.addObject("error",
-					"Sie haben keinen Zugriff auf dieser Anwendung.");
-		}
-		model.setViewName("/login");
+        return model;
+    }
 
-		return model;
-	}
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView login(
+            @RequestParam(value = "error", required = false) String error,
+            @RequestParam(value = "logout", required = false) String logout,
+            @RequestParam(value = "autherror", required = false) String authError,
+            HttpServletRequest request) throws ServletException {
+        logger.debug("Login executed");
+
+        ModelAndView model = new ModelAndView();
+        if (error != null) {
+            model.addObject("error", "Benutzername und/oder Passwort falsch!");
+        }
+        if (logout != null) {
+            request.logout();
+            model.addObject("msg", "Logout war erfolgreich.");
+        }
+        if (authError != null) {
+            request.logout();
+            model.addObject("error",
+                    "Sie haben keinen Zugriff auf dieser Anwendung.");
+        }
+        model.setViewName("/login");
+
+        return model;
+    }
 
 }

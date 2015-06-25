28e8d1d4beffdd13c1eeedb779e56ca30429f691
idueppe <ingo.dueppe@dueppe.com>
2015-06-25 02:20:33 +0200

    Created ReportEngineFactory and a ReportConfig Class

---
+++ src/main/java/de/interseroh/report/server/service/BirtEngineFactory.java
@@ -0,0 +1,65 @@
+package de.interseroh.report.server.service;
+
+import org.eclipse.birt.core.exception.BirtException;
+import org.eclipse.birt.core.framework.Platform;
+import org.eclipse.birt.report.engine.api.EngineConfig;
+import org.eclipse.birt.report.engine.api.IReportEngine;
+import org.eclipse.birt.report.engine.api.IReportEngineFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.io.Resource;
+import org.thymeleaf.util.ResourcePool;
+
+/**
+ * Created by idueppe on 22.06.15.
+ */
+public class BirtEngineFactory implements FactoryBean, ApplicationContextAware, DisposableBean {
+
+    public static final String SPRING_KEY = "spring";
+    private ApplicationContext applicationContext;
+
+    private IReportEngine birtEngine;
+
+    private Resource logDirectory;
+
+    @Override
+    public Object getObject() throws Exception {
+        EngineConfig config = new EngineConfig();
+        config.getAppContext().put(SPRING_KEY, this.applicationContext);
+
+        try {
+            Platform.startup(config);
+        } catch (BirtException be) {
+            throw new RuntimeException("Could not start the Birt engine!",be);
+        }
+
+        IReportEngineFactory factory = (IReportEngineFactory) Platform.createFactoryObject(IReportEngineFactory.EXTENSION_REPORT_ENGINE_FACTORY);
+        birtEngine = factory.createReportEngine(config);
+
+        return birtEngine;
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return IReportEngine.class;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return true;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        birtEngine.destroy();
+        Platform.shutdown();
+    }
+}
---
+++ src/main/java/de/interseroh/report/webconfig/ReportConfig.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * 
+ * (c) 2015 - Interseroh
+ */
+package de.interseroh.report.webconfig;
+
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import de.interseroh.report.server.service.BirtEngineFactory;
+import de.interseroh.report.util.RestProviderBeanScanner;
+import de.interseroh.report.util.RestServiceBeanScanner;
+import org.apache.cxf.bus.spring.SpringBus;
+import org.apache.cxf.endpoint.Server;
+import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.ext.RuntimeDelegate;
+import java.util.List;
+
+@Configuration
+@ComponentScan("de.interseroh.report.server.service")
+@PropertySource("classpath:config.properties")
+public class ReportConfig {
+
+	@Bean
+	public BirtEngineFactory birtEngineFactory() {
+		return new BirtEngineFactory();
+	}
+
+
+}

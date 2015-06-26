6080391f69bbab092f761b97581ff539d1b71250
idueppe <ingo.dueppe@dueppe.com>
2015-06-26 11:14:45 +0200

    Add ApplicationException

---
+++ src/main/java/de/interseroh/report/server/exception/ApplicationException.java
@@ -0,0 +1,27 @@
+package de.interseroh.report.server.exception;
+
+/**
+ * @author Ingo Düppe (Crowdcode)
+ */
+public class ApplicationException extends Exception {
+
+    public ApplicationException() {
+    }
+
+    public ApplicationException(String message) {
+        super(message);
+    }
+
+    public ApplicationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ApplicationException(Throwable cause) {
+        super(cause);
+    }
+
+    public ApplicationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+}

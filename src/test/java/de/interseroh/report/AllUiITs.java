57871ae3877100d761357e700e06202d7a7569f2
idueppe <ingo.dueppe@dueppe.com>
2015-06-28 22:49:59 +0200

    removed comment

--- src/test/java/de/interseroh/report/AllUiITs.java
+++ src/test/java/de/interseroh/report/AllUiITs.java
@@ -25,7 +25,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ // GwtTestEntsorgung.class
+@SuiteClasses({
 })
 public class AllUiITs {
 }

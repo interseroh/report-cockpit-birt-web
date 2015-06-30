384642f298a12ba242007aef6e3eff37faa5ab66
idueppe <ingo.dueppe@dueppe.com>
2015-06-30 14:18:56 +0200

    Refactor controller

--- src/main/java/de/interseroh/report/controller/LoginController.java
+++ src/main/java/de/interseroh/report/controller/LoginController.java
@@ -15,32 +15,30 @@
  * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
+ *
  * (c) 2015 - Interseroh
  */
 package de.interseroh.report.controller;
 
-import java.util.Collection;
-
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import de.interseroh.report.exception.BirtReportException;
-import de.interseroh.report.model.Parameter;
-import de.interseroh.report.services.BirtReportService;
-
+/**
+ * @author Ingo Düppe (Crowdcode)
+ */
 @Controller
 @PropertySource({ "classpath:config.properties", "classpath:version.properties" })
-public class ReportController {
+public class LoginController {
 
 	private static final Logger logger = Logger
 			.getLogger(ReportController.class);
@@ -48,32 +46,48 @@ public class ReportController {
 	@Autowired
 	private Environment env;
 
-	@Autowired
-	private BirtReportService reportService;
+	@RequestMapping(value = { "/index" }, method = RequestMethod.GET)
+	public ModelAndView index() {
+		logger.debug("Index view executed");
 
-	@RequestMapping(value = "/report/{reportName}", method = RequestMethod.GET)
-	public ModelAndView reportView(
-			@PathVariable("reportName") String reportName,
-			HttpServletResponse reponse) {
 		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("/report");
-		modelAndView.addObject("reportApiUrl", "/api/render/" + reportName);
+		modelAndView.setViewName("/index");
+
+		// TODO idueppe - use messaging instead.
+		String brandingText = env.getProperty("text.branding",
+				"Report Cockpit for Birt");
+		modelAndView.addObject("text.branding", brandingText);
+
+		String version = env.getProperty("version");
+		modelAndView.addObject("version", version);
+
 		return modelAndView;
 	}
 
-	@RequestMapping(value = "/report/{reportName}/param", method = RequestMethod.GET)
-	public ModelAndView reportParameter(
-			@PathVariable("reportName") String reportName)
-			throws BirtReportException {
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ModelAndView login(
+			@RequestParam(value = "error", required = false) String error,
+			@RequestParam(value = "logout", required = false) String logout,
+			@RequestParam(value = "autherror", required = false) String authError,
+			HttpServletRequest request) throws ServletException {
+		logger.debug("Login executing...");
 
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("/parameters");
-		modelAndView.addObject("reportApiUrl", "/api/render/" + reportName);
+		ModelAndView model = new ModelAndView();
+		if (error != null) {
+			model.addObject("error", "Benutzername und/oder Passwort falsch!");
+		}
+		if (logout != null) {
+			request.logout();
+			model.addObject("msg", "Logout war erfolgreich.");
+		}
+		if (authError != null) {
+			request.logout();
+			model.addObject("error",
+					"Sie haben keinen Zugriff auf dieser Anwendung.");
+		}
+		model.setViewName("/login");
 
-		Collection<Parameter> parameters = reportService
-				.getParameterDefinitions(reportName);
-		modelAndView.addObject("parameters", parameters);
-
-		return modelAndView;
+		return model;
 	}
+
 }

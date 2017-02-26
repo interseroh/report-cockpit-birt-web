da4513db40995d220d461d4b65f384f3df1f530a
Ingo Düppe <ingo.dueppe@crowdcode.io>
2017-02-26 19:47:20 +0100

    fix test

--- src/test/java/de/interseroh/report/formatters/DateFormatterTest.java
+++ src/test/java/de/interseroh/report/formatters/DateFormatterTest.java
@@ -13,32 +13,32 @@ import static org.junit.Assert.assertThat;
  */
 public class DateFormatterTest {
 
-	private static final Date testDate = new Date(2015, 12, 13);
+	private static final Date testDate = new Date(1488063600000L);
 
     private static final DateFormatter formatter = new DateFormatter();
 
 	@Test
 	public void testParse() throws Exception {
-		Date date = formatter.parse("12/13/15", Locale.US);
+		Date date = formatter.parse("02/26/17", Locale.US);
 		assertThat(date, is(testDate));
 	}
 
 	@Test
 	public void testPrint() throws Exception {
         String print = formatter.print(testDate, Locale.US);
-		assertThat(print, is("12/13/15"));
-    }
+		assertThat(print, is("2/26/17"));
+	}
 
     @Test
     public void testParseGermanDate() throws Exception {
-        Date date = formatter.parse("13.12.2015", Locale.GERMAN);
-        assertThat(date, is(testDate));
+		Date date = formatter.parse("26.02.2017", Locale.GERMAN);
+		assertThat(date, is(testDate));
     }
 
     @Test
     public void testPrintGermanDate() throws Exception {
         String print = formatter.print(testDate, Locale.GERMAN);
-        assertThat(print, is("13.12.15"));
+		assertThat(print, is("26.02.17"));
 
     }
 }
